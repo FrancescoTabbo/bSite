c14ab8f6eea2f2f80f22ccd5e03311619fefc1c7
Francesco Tabbò <francesco.tabbo2000@gmail.com>
2020-02-14 13:14:48 +0000

    graphic changes

--- js/main.js
+++ js/main.js
@@ -29,11 +29,6 @@ $(function() {
 
   const addParticipantsMessage = (data) => {
     var message = '';
-    if (data.numUsers === 1) {
-      message += "there's 1 participant";
-    } else {
-      message += "there are " + data.numUsers + " participants";
-    }
     log(message);
   }
 
